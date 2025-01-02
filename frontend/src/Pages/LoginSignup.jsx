e45e66fe3cebb5fb50b8ceb4b7021592b3ced70c
Aaron Gao <gaoaaron1@gmail.com>
2025-01-02 00:26:02 -0700

    Updated frontend

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -18,7 +18,7 @@ const LoginSignup = () => {
     const login = async () => {
         console.log("Login Function Executed", formData);
         let responseData;
-        await fetch('https://ruby-red-backend.vercel.app/login', {  // Updated backend URL
+        await fetch('http://localhost:4000/login', {
             method:'POST',
             headers: {
                 Accept:'application/form-data',
@@ -35,10 +35,12 @@ const LoginSignup = () => {
         }
     }    
     
+    
+
     const signup = async () => {
         console.log("Signup Function Executed", formData);
         let responseData;
-        await fetch('https://ruby-red-backend.vercel.app/signup', {  // Updated backend URL
+        await fetch('http://localhost:4000/signup', {
             method:'POST',
             headers: {
                 Accept:'application/form-data',
@@ -57,20 +59,21 @@ const LoginSignup = () => {
 
     return (
         <div className="loginsignup">
+            
             <div className="loginsignup-container">
                 <video className="about-video" autoPlay loop muted>
                     <source src={AboutVideo} type="video/mp4" />
                 </video>
                 <h1>{state}</h1>
                 <div className="loginsignup-fields">
-                    {state==="Sign Up" ? <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" /> : <></>}
+                    {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" />:<></>}
                     <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address' />
                     <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
                 </div>
-                <button onClick={() => {state === "Login" ? login() : signup()}}>Continue</button>
-                {state === "Sign Up"
-                ? <p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>
-                : <p className="loginsignup-login">Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
+                <button onClick={() => {state==="Login"?login():signup()}}>Continue</button>
+                {state==="Sign Up"
+                ?<p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>
+                :<p className="loginsignup-login">Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
 
                 <div className="loginsignup-agree">
                     <input type="checkbox" name='' id='' />
