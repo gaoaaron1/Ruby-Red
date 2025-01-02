314dff99253a160535fa5d4c46734b231ab5dd22
Aaron Gao <gaoaaron1@gmail.com>
2025-01-01 23:52:02 -0700

    vercel deployment test

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-const port = 4000;
+const port = process.env.PORT || 4000;
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -34,7 +35,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'),(req,res)=>{
     res.json({
         success: 1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${process.env.VERCEL_URL}/images/${req.file.filename}`
     })
 })
 
